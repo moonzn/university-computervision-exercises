a45ed1448be29f41fb0ad9c916a564c6700fa5ac
moonzn <raulmn.biz@gmail.com>
2023-09-29 18:12:36 +0100

    refactoring and challenge 1 dir

---
+++ challenges/beach_identifier/beach_identifier.py
@@ -0,0 +1,23 @@
+# Beach classifier
+directory = "images/"
+
+
+def classifier(classification):
+    print("i'm the classifier")
+    # return classification
+
+
+def prediction_result():
+    print("image with real classification vs prediction")
+
+
+def metrics():
+    print("ratio (corrects/total)")
+
+
+def main():
+    print("main func")
+
+
+if __name__ == "__main__":
+    main()
