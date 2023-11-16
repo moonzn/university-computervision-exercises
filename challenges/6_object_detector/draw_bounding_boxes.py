d21ad87a331afb06df8309914eaafd95ba28876a
moonzn <raulmn.biz@gmail.com>
2023-11-16 17:53:42 +0000

    final comments

--- challenges/6_object_detector/draw_bounding_boxes.py
+++ challenges/6_object_detector/draw_bounding_boxes.py
@@ -1,11 +1,41 @@
+"""
+APVC - Challenge 6 (Performance of a cat detector)
+
+This file implements the reading and visualization of the images from the dataset with the ground truth of the cats,
+read from the annotations of each image.
+This file also helps calculate the IoU (Intersection over Union) metric between two boxes.
+
+Instructions:
+• To run this program, you need to place a "dataset" and a "config" directory in the same directory as this script.
+• The directory should be organized as follows:
+    - config
+        - object_detection_classes_coco.txt
+        - yolov5s.onnx
+    - dataset
+        - images
+            - cats_01.jpg
+            - cats_02.jpg
+            (...)
+        - annotations
+            - cats_01.xml
+            - cats_02.xml
+            (...)
+
+Authors:
+• Bernardo Grilo, n.º 93251
+• Gonçalo Carrasco, n.º 109379
+• Raúl Nascimento, n.º 87405
+"""
+
 import os
 import cv2 as cv
 import xml.etree.ElementTree as ET
 
-IMAGES_PATH = "dataset/images"
-ANNOT_PATH = "dataset/annotations"
+IMAGES_PATH = "dataset/images"  # Directory with cat pictures
+ANNOT_PATH = "dataset/annotations"  # Directory with the annotations of the bounding boxes of the cat images
 
 
+# Reading the folder containing the images.
 def load_images():
     images = []
     for filename in os.listdir(IMAGES_PATH):
@@ -15,6 +45,7 @@ def load_images():
     return images
 
 
+# Obtaining the ground truth coordinates of the images from the annotation files
 def load_bounding_boxes():
     boxes = []
     total = 0
@@ -26,17 +57,18 @@ def load_bounding_boxes():
     return boxes, total
 
 
+# Reading the ground truth coordinates of an image from its XML annotation file
 def parse_xml(xml):
-    # create element tree object
+    # Create element tree object
     tree = ET.parse(xml)
 
-    # get root element
+    # Get root element
     root = tree.getroot()
 
-    # stores all boxes of an img
+    # Stores all boxes of an img
     boxes_in_img = []
 
-    # counts boxes
+    # Counts boxes
     count = 0
 
     for box in root.findall("./object/bndbox"):
@@ -49,28 +81,28 @@ def parse_xml(xml):
     return boxes_in_img, count
 
 
+# Drawing the bounding boxes on an image
 def draw_bounding_boxes(img, boxes):
+    color = (135, 135, 135)
+    # Line thickness of 2 px
+    thickness = 2
+
     for coords in boxes:
-        # represents the top left corner of rectangle
+        # Represents the top left corner of rectangle
         start_point = (int(coords[0]), int(coords[1]))
-
-        # represents the bottom right corner of rectangle
+        # Represents the bottom right corner of rectangle
         end_point = (int(coords[2]), int(coords[3]))
-
-        # Blue color in BGR
-        color = (135, 135, 135)
-
-        # Line thickness of 2 px
-        thickness = 2
 
         img = cv.rectangle(img, start_point, end_point, color, thickness)
     return img
 
 
+# Converting the elements of a list with numeric strings to integer values
 def cast_list_to_int(lst):
     return list(map(int, lst))
 
 
+# Calculation of the IoU metric between two boxes (ground truth and detection bounding box)
 def calculate_iou(box1, box2):
     x_inter1 = max(box1[0], box2[0])
     y_inter1 = max(box1[1], box2[1])
@@ -80,12 +112,12 @@ def calculate_iou(box1, box2):
     if x_inter2 < x_inter1 or y_inter2 < y_inter1:
         return 0
 
-    # Área da interseção
+    # Intersection area
     width_inter = abs(x_inter2 - x_inter1)
     height_inter = abs(y_inter2 - y_inter1)
     area_inter = width_inter * height_inter
 
-    # Área da união
+    # Union area
     width_ground_truth = abs(box1[2] - box1[0])
     height_ground_truth = abs(box1[3] - box1[1])
     area_ground_truth = width_ground_truth * height_ground_truth
@@ -94,7 +126,6 @@ def calculate_iou(box1, box2):
     area_detection = width_prediction * height_prediction
     area_union = area_detection + area_ground_truth - area_inter
 
-    # Cálculo do IoU
     return area_inter / area_union
 
 
@@ -105,6 +136,7 @@ def main():
     for idx in range(len(images)):
         images[idx] = draw_bounding_boxes(images[idx], boxes[idx])
 
+    # Display of the images with the rectangles (bounding boxes of the annotations)
     for img in images:
         cv.imshow("Bounding Boxes", img)
         cv.waitKey(0)
--- challenges/6_object_detector/yolov5_cat_detection.py
+++ challenges/6_object_detector/yolov5_cat_detection.py
@@ -1,31 +1,57 @@
+"""
+APVC - Challenge 6 (Performance of a cat detector)
+
+This file implements the detection of cats in the images of the dataset, using Yolo v5.
+
+Instructions:
+• To run this program, you need to place a "dataset" and a "config" directory in the same directory as this script.
+• The directory should be organized as follows:
+    - config
+        - object_detection_classes_coco.txt
+        - yolov5s.onnx
+    - dataset
+        - images
+            - cats_01.jpg
+            - cats_02.jpg
+            (...)
+        - annotations
+            - cats_01.xml
+            - cats_02.xml
+            (...)
+
+Authors:
+• Bernardo Grilo, n.º 93251
+• Gonçalo Carrasco, n.º 109379
+• Raúl Nascimento, n.º 87405
+"""
+
 import cv2
 import numpy as np
-from draw_bounding_boxes import load_images, load_bounding_boxes, cast_list_to_int, calculate_iou, draw_bounding_boxes
+from draw_bounding_boxes import load_images, load_bounding_boxes, cast_list_to_int, calculate_iou, \
+    draw_bounding_boxes
 
 MODEL_FILE = "config/yolov5s.onnx"
 CLASS_FILE = "config/object_detection_classes_coco.txt"
 
-DATASET_PATH = "dataset/images"  # Caminho para as imagens a analisar
-ANNOT_PATH = "dataset/annotations"  # Caminho para as anotações das imagens
+DATASET_PATH = "dataset/images"  # Path to the images
 
-CONFIDENCE_THRESHOLD = 0.33  # Threshold para a confianca nas bounding boxes
-NMS_THRESHOLD = 0.4  # Threshold para o algoritmo non maximum supression
-IOU_THRESHOLD = 0.5  # Threshold para o IoU
+CONFIDENCE_THRESHOLD = 0.33  # Threshold for trust in bounding boxes
+NMS_THRESHOLD = 0.4  # Threshold for the Non-maximum Suppression algorithm
+IOU_THRESHOLD = 0.5  # Threshold for the IoU metric
 
-WIDTH = 640   # largura da imagem no YOLOV5
-HEIGHT = 640  # altura da imagem no YOLOV5
+WIDTH = 640   # Image width on Yolo v5
+HEIGHT = 640  # Image height on Yolo v5
 
-# ler os nomes das classes
+# Reading class names
 with open(CLASS_FILE, 'r') as f:
     class_names = f.read().split('\n')
 
-# gerar cores aleatoriamente para cada uma das classes
+# Randomly generate colors for each class
 COLORS = np.random.uniform(0, 255, size=(len(class_names), 3))
 
-# carregar o modelo, neste caso o YOLO
+# Load the model, in this case Yolo v5
 YoloModel = cv2.dnn.readNet(MODEL_FILE)
 YoloModel.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-# YoloModel.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
 images = load_images()
 boxes, total_boxes = load_bounding_boxes()
@@ -38,7 +64,7 @@ for idx in range(len(images)):
     img_boxes = boxes[idx]
     img_height, img_width, channels = img.shape
 
-    # Fator de conversão (dimensões reais da imagem para dimensões lidas pelo Yolo)
+    # Conversion factor (actual image dimensions to dimensions read by Yolo)
     x_conversion_factor = img_width / WIDTH
     y_conversion_factor = img_height / HEIGHT
 
@@ -55,14 +81,15 @@ for idx in range(len(images)):
 
         detection = outputs[i, :]
 
-        # obter o valor da confianca da bounding box
+        # Obtain the confidence value of the bounding box
         confidence = detection[4]
 
         if confidence > CONFIDENCE_THRESHOLD:
             scores = detection[5:]
 
             classID = np.argmax(scores) + 1
-            if classID != 16:  # ID da classe 'cat'
+            # Only bounding boxes where the detected objects are cats are considered
+            if classID != 16:  # Cat class ID
                 continue
 
             bbox_center_x = detection[0] * x_conversion_factor
@@ -77,25 +104,25 @@ for idx in range(len(images)):
             confidences.append(float(confidence))
             classIDs.append(classID)
 
-    # Remover bounding boxes adicionais usando non maximum suppression
+    # Remove additional bounding boxes using the Non-maximum Suppression algorithm
     idxs = cv2.dnn.NMSBoxes(bboxes, confidences, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
 
     if len(idxs) > 0:
         for i in idxs.flatten():
             iou_values = []
 
-            # extrair as coordenadas e dimensoes das bounding boxes resultantes
+            # Extract the coordinates and dimensions of the resulting bounding boxes
             (bbox_x, bbox_y) = (bboxes[i][0], bboxes[i][1])
             (bbox_w, bbox_h) = (bboxes[i][2], bboxes[i][3])
 
-            # colocar retangulos e texto a marcar os objetos identificados
+            # Place rectangles and text to mark the identified objects
             class_name = class_names[classIDs[i]]
             color = (189, 240, 38)
             cv2.rectangle(img, (bbox_x, bbox_y), (bbox_x + bbox_w, bbox_y + bbox_h), color, 2)
             text = "{}: {:.4f}".format(class_name, confidences[i])
             cv2.putText(img, text, (bbox_x, bbox_y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
 
-            # Cálculo das métricas
+            # Calculating metrics
             for coord in img_boxes:
                 prediction_box = [bbox_x, bbox_y, bbox_x + bbox_w, bbox_y + bbox_h]
                 iou = calculate_iou(cast_list_to_int(coord), prediction_box)
@@ -112,10 +139,11 @@ for idx in range(len(images)):
     # Bounding Boxes from Annotations
     img = draw_bounding_boxes(img, boxes[idx])
 
-    precision = true_positives/(true_positives + false_positives)
-    recall = true_positives/total_boxes
-    average_iou = sum(max_ious)/len(max_ious)
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / total_boxes
+    average_iou = sum(max_ious) / len(max_ious)
 
+    # Display of metrics in relation to the total bounding boxes of the entire dataset
     print(f"After {idx+1} images:")
     print(f"Precision: {round(precision, 3)}"
           f" | Recall: {round(recall, 3)}"
